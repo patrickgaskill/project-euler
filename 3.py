e734fb122724e10059851aa0126efd6da0dd912e
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-02-04 14:43:07 -0500

    Better prime checking

--- 3.py
+++ 3.py
@@ -1,9 +1,10 @@
 from math import sqrt
+from itertools import count, islice
 
 def is_prime(n):
-    return all(n % i for i in range(2, n))
+    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n)-1)))
 
 def prime_factors(n):
-    return list(filter(lambda x: n % x == 0 and is_prime(x), range(2, int(sqrt(n) / 2))))
+    return list(filter(lambda x: n % x == 0 and is_prime(x), islice(count(2), int(sqrt(n)-1))))
 
 print(prime_factors(600851475143)[-1])
