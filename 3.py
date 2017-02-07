03a35895dbf71caa2ddd5b9f6f623f41b0160241
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-02-07 15:56:43 -0500

    Updated to use my is_prime

--- 3.py
+++ 3.py
@@ -1,10 +1,7 @@
-from math import sqrt
-from itertools import count, islice
-
-def is_prime(n):
-    return n > 1 and all(n % i for i in islice(count(2), int(sqrt(n)-1)))
+from itertools import islice, count
+from patrick import is_prime
 
 def prime_factors(n):
-    return list(filter(lambda x: n % x == 0 and is_prime(x), islice(count(2), int(sqrt(n)-1))))
+    return list(filter(lambda x: n % x == 0 and is_prime(x), islice(count(2), int(n ** 0.5 - 1))))
 
 print(prime_factors(600851475143)[-1])
