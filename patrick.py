a2cbafd652cfe96e38147128998c2857abe9b975
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-02-07 15:57:58 -0500

    Removes parens from if statements

--- patrick.py
+++ patrick.py
@@ -1,21 +1,21 @@
 def is_prime(n):
-    if (n == 2):
+    if n == 2:
         return True
 
-    if (n == 3):
+    if n == 3:
         return True
 
-    if (n % 2 == 0):
+    if n % 2 == 0:
         return False
 
-    if (n % 3 == 0):
+    if n % 3 == 0:
         return False
 
     i = 5
     w = 2
 
     while i * i <= n:
-        if (n % i == 0):
+        if n % i == 0:
             return False
 
         i += w
