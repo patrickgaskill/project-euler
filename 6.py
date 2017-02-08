03d6d2f9278821aebb52a929f0d889148980254d
Patrick Gaskill <patrick.gaskill@gmail.com>
2017-02-08 14:06:13 -0500

    Converted lambda to list comprehension

--- 6.py
+++ 6.py
@@ -1,4 +1,4 @@
 NUMS = 100
-sum_of_squares = sum(map(lambda x: x ** 2, range(NUMS + 1)))
+sum_of_squares = sum(x ** 2 for x in range(NUMS + 1))
 square_of_sum = sum(range(NUMS + 1)) ** 2
 print(square_of_sum - sum_of_squares)
